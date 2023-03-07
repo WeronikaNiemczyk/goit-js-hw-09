aa5092e03a9433037caf1ced19e51a7f0e62730a
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-03-07 20:57:13 +0100

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+intervalID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(getRandomHexColor());
+
+function onStartBtnChangeColor() {
+  intervalID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onStopBtnNoChangeColor() {
+  clearInterval(intervalID);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', onStartBtnChangeColor);
+stopBtn.addEventListener('click', onStopBtnNoChangeColor);
